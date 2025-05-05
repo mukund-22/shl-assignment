d89bc7ae1d2732221f8a3d857bda296c1dd5fe9c
mukund-22 <mukundvas22@gmail.com>
2025-05-05 10:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def recommend_assessments(request: QueryRequest):
                 )
             )
         return RecommendResponse(recommended_assessments=recommended_assessments)
-
 if __name__ == "__main__":
-        uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    # Get the port from environment variable (or default to 10000)
+    port = int(os.environ.get("PORT", 10000))  
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
