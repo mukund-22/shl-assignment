d3ba8bea9022e7a258df7b8c9a8d00b4b09b7b47
mukund-22 <mukundvas22@gmail.com>
2025-05-05 11:52:53 +0530

    Update _metadata_requests.py

--- utils/_metadata_requests.py
+++ utils/_metadata_requests.py
@@ -83,7 +83,7 @@ from copy import deepcopy
 from typing import TYPE_CHECKING, Optional, Union
 from warnings import warn
 
-from .. import get_config
+from utils._config import get_config
 from ..exceptions import UnsetMetadataPassedError
 from ._bunch import Bunch
 
